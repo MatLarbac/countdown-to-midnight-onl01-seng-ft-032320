2c947906a094cf4953a4229d3dc18fd4be42bc0b
Mat Cabral <mathewcabral@gmail.com>
2020-03-26 15:53:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
 def countdown
+num = 10
+  while num > 0
+   
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+ 
+ puts countdown
+def countdown_with_sleep
+  
+
+num = 10
+  while num > 0
+ 
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep  
+    end
+    return "HAPPY NEW YEAR!"
+end
+puts countdown_with_sleep
